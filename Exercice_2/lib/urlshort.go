e3697f0fab5a80c093dfe1593ffb2c6937ad44fa
Romain Leclere <romain.leclere@etu.utc.fr>
2020-04-01 22:09:58 +0200

    exercice_2

---
+++ Exercice_2/lib/urlshort.go
@@ -0,0 +1,65 @@
+package urlshort
+
+import (
+	"fmt"
+	Yaml "github.com/go-yaml/yaml"
+	"net/http"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+type mapPath struct {
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"`
+}
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var mappath []mapPath
+	err := Yaml.Unmarshal(yml, &mappath)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(mappath)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func buildMap(val []mapPath) map[string]string {
+	paths := make(map[string]string)
+	for _, pu := range val {
+		paths[pu.Path] = pu.Url
+		fmt.Printf("%s -> %s \n", pu.Path, pu.Url)
+	}
+	return paths
+}
