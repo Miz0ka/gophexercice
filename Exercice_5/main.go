1ac25a7ca70557c716c7834961e9306419af6f63
Romain Leclere <romain.leclere@etu.utc.fr>
2020-04-11 20:38:27 +0200

    Exercice 5 fix

--- Exercice_5/main.go
+++ Exercice_5/main.go
@@ -8,6 +8,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 type SiteMap struct {
@@ -18,38 +20,18 @@ type SiteMap struct {
 func main() {
 	fmt.Println("Exercice 5")
 
-	var urlBase, url string
+	var urlBase string
 
 	flag.StringVar(&urlBase, "url", "https://www.calhoun.io/", "Url to the site to parse")
 	flag.Parse()
 
 	//var sitemap SiteMap
-	linktoexplore := make([]string, 0)
 	linkvisited := make([]string, 0)
+	domaine := domainExtract(urlBase)
 
-	linktoexplore = append(linktoexplore, urlBase)
+	linkvisited = append(linkvisited, "")
 
-	for len(linktoexplore) != 0 {
-		url, linktoexplore = linktoexplore[len(linktoexplore)-1], linktoexplore[:len(linktoexplore)-1]
-		linkvisited = append(linkvisited, url)
-		res, err := http.Get(url)
-		if err != nil {
-			fmt.Println("Can't access to :", url)
-		}
-		if res.StatusCode == 200 {
-			newurl := htmlparser.SearchALink(res.Body)
-			for _, val := range newurl {
-				fmt.Println(val.Href)
-			}
-			//Check URL(Domain + already visited)
-		} else {
-			log.Println(url, " failed to acces : ", res.StatusCode)
-		}
-	}
-
-	sitemap := &SiteMap{
-		Url: urlBase,
-	}
+	sitemap := exploreSite(urlBase, &linkvisited, domaine)
 
 	file, err := xml.MarshalIndent(sitemap, "", " ")
 	file = []byte(xml.Header + string(file))
@@ -59,3 +41,57 @@ func main() {
 
 	_ = ioutil.WriteFile("sitemap.xml", file, 0644)
 }
+
+func exploreSite(current string, linkvisited *[]string, domaine string) SiteMap {
+	sitemap := &SiteMap{
+		Url:      current,
+		Sitemaps: make([]SiteMap, 0),
+	}
+	*linkvisited = append(*linkvisited, current)
+	res, err := http.Get(current)
+	if err != nil {
+		fmt.Println("Can't access to :", current)
+	}
+	if res.StatusCode == 200 {
+		newurl := htmlparser.SearchALink(res.Body)
+		for _, val := range newurl {
+			nexturl := checkUrl(val.Href, domaine)
+			if !contains(*linkvisited, nexturl) {
+				undersite := exploreSite(nexturl, linkvisited, domaine)
+				sitemap.Sitemaps = append(sitemap.Sitemaps, undersite)
+				fmt.Println(val.Href)
+			}
+		}
+		//Check URL(Domain + already visited)
+	} else {
+		log.Println(current, " failed to acces : ", res.StatusCode)
+	}
+	return *sitemap
+}
+
+func domainExtract(url string) string {
+	re := regexp.MustCompile(`(https?:\/\/.*\/)`)
+	return re.FindStringSubmatch(url)[0]
+}
+
+func checkUrl(url string, domaine string) string {
+	if strings.Contains(url, domaine) {
+		return url
+	}
+	re := regexp.MustCompile(`$\/`)
+	if re.Match([]byte(url)) {
+		return domaine + url
+	}
+	return ""
+}
+
+func contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+//TODO correctly manage domain
